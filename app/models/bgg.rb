499e7925ce863afe56f3bf4f96cecde11af33168
Matthew Lockdall <mlockdall26@gmail.com>
2019-11-03 10:12:23 -0600

    updated cors whitelist

--- app/models/bgg.rb
+++ app/models/bgg.rb
@@ -3,13 +3,13 @@ class Bgg
   def initialize gameid
     @gameid = gameid
   end
-
+  # playing with built in pagination
   def get_bgg_game
     response = HTTParty.get('https://www.boardgamegeek.com/xmlapi2/thing?id='+ @gameid)
   end
 
   def get_bgg_list
-    response = HTTParty.get('https://www.boardgamegeek.com/xmlapi2/geeklist/252877')
+    response = HTTParty.get('https://www.boardgamegeek.com/xmlapi/geeklist/252877')
   end
 
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,13 +1,11 @@
 class Search
-  def initialize query 
 
+  def initialize query
     @query = query
-
-
   end
 
   def get_search
-    HTTParty.get('https://www.boardgamegeek.com/xmlapi2/search?query='+ @query)
+    HTTParty.get('https://www.boardgamegeek.com/xmlapi2/search?query='+ @query + '&pagesize=15&page=1')
   end
 
 end
